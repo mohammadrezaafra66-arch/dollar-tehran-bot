2b1b4cd555a54faa101f4bf075134d620e86301e
mohammadrezaafra66-arch <mohammadrezaafra66@gmail.com>
2026-05-18 19:22:34 +0330

    Point dashboard to modular Afra package

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,4 @@
 import uvicorn
 
-if __name__ == "__main__":
-    uvicorn.run("dollar_bot.web:app", host="127.0.0.1", port=8090, reload=False)
+if __name__ == '__main__':
+    uvicorn.run('afra_market_data.dashboard:app', host='127.0.0.1', port=8090, reload=False)
