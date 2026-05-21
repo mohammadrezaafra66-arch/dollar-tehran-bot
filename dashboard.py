0d79fb6541b817f429f507d2fd1f8adc08691e2f
mohammadrezaafra66-arch <mohammadrezaafra66@gmail.com>
2026-05-21 13:04:13 +0330

    Make dashboard host configurable

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,9 @@
+import os
+
 import uvicorn
 
+
 if __name__ == '__main__':
-    uvicorn.run('afra_market_data.dashboard:app', host='127.0.0.1', port=8090, reload=False)
+    host = os.getenv('AFRA_DASHBOARD_HOST', '127.0.0.1')
+    port = int(os.getenv('AFRA_DASHBOARD_PORT', '8090'))
+    uvicorn.run('afra_market_data.dashboard:app', host=host, port=port, reload=False)
