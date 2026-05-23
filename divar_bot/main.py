ea39f119d8765f16625d69e1ba3cbffadf1f13e2
mohammadrezaafra66-arch <mohammadrezaafra66@gmail.com>
2026-05-24 00:06:24 +0330

    Move main runtime to divar_bot package

---
+++ divar_bot/main.py
@@ -0,0 +1,23 @@
+from divar_bot.core.queue import QueueManager
+from divar_bot.core.worker import Worker
+
+
+def bootstrap():
+    queue = QueueManager()
+
+    queue.add_job(
+        job_type='divar_extract',
+        plugin_name='divar',
+        payload={
+            'url': 'https://divar.ir'
+        },
+        priority=10,
+        speed_profile='test'
+    )
+
+    worker = Worker()
+    worker.process_next_job()
+
+
+if __name__ == '__main__':
+    bootstrap()
