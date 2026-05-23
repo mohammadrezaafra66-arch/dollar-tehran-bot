a5c1c0c2d3b19664329470ace89f07d3f02890b1
mohammadrezaafra66-arch <mohammadrezaafra66@gmail.com>
2026-05-23 22:23:33 +0330

    Move Divar plugin to divar_bot package

---
+++ divar_bot/plugins/divar/plugin.py
@@ -0,0 +1,18 @@
+from divar_bot.core.base_plugin import BasePlugin
+from divar_bot.plugins.divar.driver import DivarDriver
+
+
+class DivarPlugin(BasePlugin):
+    name = 'divar'
+
+    def __init__(self):
+        self.driver = DivarDriver()
+
+    def start(self):
+        self.driver.connect()
+
+    def stop(self):
+        self.driver.close()
+
+    def extract(self, url):
+        return self.driver.extract(url)
