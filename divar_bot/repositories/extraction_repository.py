7539df79a853b17251abed9f479c6fa8c3376292
mohammadrezaafra66-arch <mohammadrezaafra66@gmail.com>
2026-05-24 00:07:45 +0330

    Move extraction repository to divar_bot package

---
+++ divar_bot/repositories/extraction_repository.py
@@ -0,0 +1,28 @@
+import json
+from divar_bot.db.sqlite import SQLiteManager
+
+
+class ExtractionRepository:
+    def __init__(self, db=None):
+        self.db = db or SQLiteManager()
+
+    def save(self, plugin_name, data):
+        with self.db.connection() as conn:
+            conn.execute(
+                '''
+                INSERT INTO extraction_results (
+                    plugin_name,
+                    source_url,
+                    raw_title,
+                    raw_payload,
+                    confidence_score
+                ) VALUES (?, ?, ?, ?, ?)
+                ''',
+                (
+                    plugin_name,
+                    data.get('source_url'),
+                    data.get('title'),
+                    json.dumps(data.get('raw_payload')),
+                    data.get('confidence_score', 0)
+                )
+            )
