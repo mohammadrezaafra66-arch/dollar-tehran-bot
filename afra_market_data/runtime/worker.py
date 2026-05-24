38bb188bc9510e30ccb28c60de5d57dc4545a19a
mohammadrezaafra66-arch <mohammadrezaafra66@gmail.com>
2026-05-24 22:07:43 +0330

    Migrate worker runtime into afra_market_data

---
+++ afra_market_data/runtime/worker.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import asyncio
+from typing import Optional
+
+from afra_market_data.core.logger import PlatformLogger
+from afra_market_data.core.queue_manager import QueueManager, QueueJob
+
+
+class WorkerRuntime:
+    def __init__(self, worker_name: str, queue_manager: QueueManager):
+        self.worker_name = worker_name
+        self.queue_manager = queue_manager
+        self.logger = PlatformLogger()
+        self.running = False
+
+    async def start(self):
+        self.running = True
+        self.logger.run_started(self.worker_name)
+
+        while self.running:
+            job: Optional[QueueJob] = self.queue_manager.get_job()
+
+            if not job:
+                await asyncio.sleep(1)
+                continue
+
+            try:
+                self.logger.activity(
+                    'job_started',
+                    worker=self.worker_name,
+                    query=job.query,
+                    platform=job.platform,
+                )
+
+                await self.process_job(job)
+
+                self.logger.activity(
+                    'job_finished',
+                    worker=self.worker_name,
+                    query=job.query,
+                )
+
+            except Exception as e:
+                self.logger.error(
+                    'job_failed',
+                    worker=self.worker_name,
+                    query=job.query,
+                    error=str(e),
+                )
+
+    async def process_job(self, job: QueueJob):
+        await asyncio.sleep(0.1)
+
+    async def stop(self):
+        self.running = False
+        self.logger.run_finished(self.worker_name)
